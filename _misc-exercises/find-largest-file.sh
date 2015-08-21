3d8f1d0284ab826b059348760de91ffc67bc8f05
John Gosset <john.gosset@gmail.com>
2015-08-21 11:08:24 -0400

    Make find-largest-file.sh executable

--- _misc-exercises/find-largest-file.sh
+++ _misc-exercises/find-largest-file.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Prints out the largest file on the filesystem
 
+min_size=$1
+
 # NOTES
 # - the find command starts it's search at the top of the filesystem hierarchy: "/"
 # - it prints out all files that are 10 megabytes or larger
 # - we use the "-exec" action to run a command for each file found by find
 # - we then pipe these results to the sort command, sorting on column 5 (filesize), in human-readable format
 # - finally, piping to "tail -1" prints the largest file found
-find / -size +10M -exec ls -lh '{}' \; 2>/dev/null | sort -k5 -h | tail -1
+find / -size +$min_size -exec ls -lh '{}' \; 2>/dev/null | sort -k5 -h | tail -1
