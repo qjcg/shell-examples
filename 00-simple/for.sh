0f9050a762092ad5d12166e5a3a7685b2a01e3b2
John Gosset <john.gosset@gmail.com>
2015-08-21 10:29:33 -0400

    Refactor for.sh for clarity

--- 00-simple/for.sh
+++ 00-simple/for.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 # Example for loop
 
-echo 'Entering Loop!'
-for f in file{1..5}; do
+echo 'for loop: iterating through a list'
+for f in file1 file2 file3 file4 file5; do
 	echo "Current file: $f"
 done
 
-# same thing in a C-style for-loop syntax
+echo
+
+# same thing as above in a C-style for-loop syntax
+echo 'for loop: C-style syntax'
 for ((f=1; f<6; f++)); do
 	echo "Current file: file$f"
 done
