7a7320683ccb16fa6cf6ecf1006635c77472a42c
John Gosset <john.gosset@gmail.com>
2015-08-21 11:28:21 -0400

    Add input validation to find-largest-file.sh

--- _misc-exercises/find-largest-file.sh
+++ _misc-exercises/find-largest-file.sh
@@ -9,4 +9,15 @@ min_size=$1
 # - we use the "-exec" action to run a command for each file found by find
 # - we then pipe these results to the sort command, sorting on column 5 (filesize), in human-readable format
 # - finally, piping to "tail -1" prints the largest file found
-find / -size +$min_size -exec ls -lh '{}' \; 2>/dev/null | sort -k5 -h | tail -1
+
+if [[ -z $1 ]]; then
+	echo You must specify a minimum filesize as the first argument.
+	exit 1
+fi
+
+# Validate input via regular expression test
+if [[ $1 =~ [1-9][0-9]*[KMGT]? ]] ; then
+	find / -size +$min_size -exec ls -lh '{}' \; 2>/dev/null | sort -k5 -h | tail -1
+else
+	echo "First argument must be a minimum filesize (ex: 10M, 1G)"
+fi
