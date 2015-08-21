e354db672819ac6d3d70e0971986a3b1a721cc5b
John Gosset <john.gosset@gmail.com>
2015-08-20 21:03:07 -0400

    Add zero-padding examples.

--- expansion_brace.sh
+++ expansion_brace.sh
@@ -4,6 +4,12 @@
 echo The numbers from one to ten via brace expansion:
 echo {1..10}
 
+echo "The numbers from one to ten via brace expansion (zero-padded):"
+echo {01..10}
+
+echo "The numbers from one to ten via brace expansion (double-zero padded):"
+echo {001..10}
+
 echo The numbers from one to ten via brace expansion, stepping by 2:
 echo {1..10..2}
 
