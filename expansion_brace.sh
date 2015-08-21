96f112daffce1f9d57b6f579b92bc73d26fa4552
John Gosset <john.gosset@gmail.com>
2015-08-21 09:03:39 -0400

    Quote strings

--- expansion_brace.sh
+++ expansion_brace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Examples of brace expansion
 
-echo The numbers from one to ten via brace expansion:
+echo "The numbers from one to ten via brace expansion:"
 echo {1..10}
 
 echo "The numbers from one to ten via brace expansion (zero-padded):"
@@ -10,11 +10,11 @@ echo {01..10}
 echo "The numbers from one to ten via brace expansion (double-zero padded):"
 echo {001..10}
 
-echo The numbers from one to ten via brace expansion, stepping by 2:
+echo "The numbers from one to ten via brace expansion, stepping by 2:"
 echo {1..10..2}
 
-echo The numbers from ten to one via brace expansion, stepping by -1:
+echo "The numbers from ten to one via brace expansion, stepping by -1:"
 echo {10..1..-1}
 
-echo The numbers from ten to one via brace expansion, stepping by -2:
+echo "The numbers from ten to one via brace expansion, stepping by -2:"
 echo {10..1..-2}
