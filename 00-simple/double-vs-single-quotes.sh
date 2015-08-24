555c86297a0ae59cdba085669e0b8b2a308326f1
John Gosset <john.gosset@gmail.com>
2015-08-24 14:37:54 -0400

    Add double-vs-single-quotes.sh

---
+++ 00-simple/double-vs-single-quotes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Illustrates the difference between double and single quotes.
+
+USERNAME='Jerry Q. Hacker'
+
+echo Variable expansion occurs within double quotes:
+echo "$USERNAME"
+
+echo Everything in single quotes is interpreted as a LITERAL STRING:
+echo '$USERNAME'
