35eca5dabb58633a7991f52495d15a3767375529
John Gosset <john.gosset@gmail.com>
2015-08-24 11:24:36 -0400

    Add awk examples

---
+++ awk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Demonstrate some simple awk functionality.
+
+# regular expression match (kind of like "grep")
+# if no action specified, will print the whole line ("$0" in awk syntax)
+awk -F: '/root/' /etc/passwd
+
+# print only 3rd and 5th fields (columns)
+awk -F: '/root/ {print $3, $5}' /etc/passwd
+
+# same thing, reverse order
+awk -F: '/root|nobody/ {print $5, $3}' /etc/passwd
