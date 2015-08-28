f0bc08d91767faec1fd1180939c8d8589f914ed4
John Gosset <john.gosset@gmail.com>
2015-08-28 18:31:16 -0400

    Split up for.sh

--- 00-fundamentals/for.sh
+++ 00-fundamentals/for.sh
@@ -5,10 +5,3 @@ echo 'for loop: list-style syntax'
 for f in file1 file2 file3 file4 file5; do
 	echo "Current file: $f"
 done
-
-echo
-
-echo 'for loop: C-style syntax'
-for ((f=1; f<6; f++)); do
-	echo "Current file: file$f"
-done
---
+++ 00-fundamentals/for_c-style.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Example for loop using the C-style syntax
+
+echo 'for loop: C-style syntax'
+for ((f=1; f<6; f++)); do
+	echo "Current file: file$f"
+done
