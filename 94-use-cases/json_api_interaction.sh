85af45596d669062d19cb1b4d47a11c2711209a9
John Gosset <john.gosset@gmail.com>
2015-09-02 16:14:30 -0400

    Update comment

--- 94-use-cases/json_api_interaction.sh
+++ 94-use-cases/json_api_interaction.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # Example of parsing JSON output from a HTTP request using jq.
 
+# This script makes use of the MediaWiki action API:
+#	https://www.mediawiki.org/wiki/API:Main_page
+# To experiment with building queries interactively, see:
+#	https://en.wikipedia.org/wiki/Special:ApiSandbox
+
 hash jq 2>/dev/null || { >&2 echo "jq must be installed: https://stedolan.github.io/jq/" ; exit 1; }
 
 # Sending a GET returns 10 random Wikipedia articles (JSON)
-# For API documentation, see https://www.mediawiki.org/wiki/API:Main_page
-# To experiement with building queries, see https://en.wikipedia.org/wiki/Special:ApiSandbox
 url='https://en.wikipedia.org/w/api.php?action=query&list=random&rnlimit=10&rnnamespace=0&format=json'
 curl -s $url | jq -r '.query .random [] .title'
