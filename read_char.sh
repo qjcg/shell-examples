44f52f0bbd3ad611531d260436a4968f9eebe0d5
John Gosset <john@gossetx.com>
2019-03-14 18:07:43 -0400

    Add read_char.sh

---
+++ read_char.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Example script that performs actions based on single character input.
+
+# REACTIVATE display of input characters and exit.
+trap "stty echo; exit 0" QUIT
+
+printf 'Keys: a b c\nCtrl-C to quit!\n\n'
+
+# DEACTIVATE display of input characters.
+stty -echo
+while read -n 1 char; do
+	case $char in
+	a) echo Command: AAA ;;
+	b) echo Command: BBB ;;
+	c) echo Command: CCC ;;
+	*) echo Command: --UNKNOWN-- ;;
+	esac
+done < /dev/stdin
