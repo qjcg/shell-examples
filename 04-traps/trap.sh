2c11cb58ad5f80a860b68b37d5d0c6c41ac2eaf7
John Gosset <john@gossetx.com>
2019-03-14 23:16:09 -0400

    Tweak trap.sh

--- 04-traps/trap.sh
+++ 04-traps/trap.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
-# Example of using the trap builtin command.
-# See "help trap"
+# Example of using the trap builtin command to intercept signals and act on them.
+# © John Gosset 2019
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Refs: "help trap".
 
-echo "SIGINT (^C) is trapped, when I receive it I'll print the date."
-echo
+cat <<!
+
+TRAPPED CATS
+
+The cat command will wait for input
+indefinitely, never terminating on it's
+own.
+
+SIGINT (^C) is trapped. Type ^C^C^C to
+terminate 3 cat commands below and then
+exit.
+
+Notice that a command of our choice is
+run on SIGINT because of the trap.
+
+!
 
 trap "echo SIGINT received!" INT
+cat
+cat
 cat
