59a2d75cf1376cab1593dffc383093c600b59a87
John Gosset <john@gossetx.com>
2019-03-14 22:52:19 -0400

    Tweak dialog.sh

--- 98-tui/dialog.sh
+++ 98-tui/dialog.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# dialog(1) user interface demo
+# dialog(1) user interface demo.
 
-hash dialog 2>/dev/null || { >&2 echo "dialog must be installed" ; exit 1; }
+command -v dialog >/dev/null || { >&2 echo "dialog must be installed" ; exit 1 ; }
 
 name=$(dialog --stdout --inputbox 'Please enter your name' 0 0)
 email=$(dialog --stdout --inputbox 'Please enter your email address' 0 0)
 birthday=$(dialog --stdout --calendar 'Please enter your birthday' 0 0 17 08 1982)
+awesome=$(dialog --stdout --yesno 'Is this awesome or what?' 0 0 && echo YES || echo NO)  # Returns exit status only.
 
 # A `clear` after taking dialog input is always a good idea, since
 # otherwise your screen stays filled with blue...
@@ -14,3 +15,4 @@ clear
 echo Name: $name
 echo Email: $email
 echo Birthday: $birthday
+echo Awesome: $awesome
